2ee8ee8835a5cae528ac7a93b313db0e91cdce72
ShiftyFella <31674713+ShiftyFella@users.noreply.github.com>
2017-09-12 22:08:48 -0400

    Added console logging of package.json file

--- index.js
+++ index.js
@@ -8,3 +8,12 @@ var server = http.createServer(function (request, response) {
 
 server.listen(8000, '127.0.0.1');
 console.log("Server listens on 127.0.0.1:8000");
+
+fs = require('fs');
+fs.readFile('package.json', 'utf8', function (err, data) {
+    if (err) {
+        return console.log(err);
+    }
+    console.log(data);
+
+});
