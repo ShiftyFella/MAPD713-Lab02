f9135d12fb495b24c21a91c640f8c1ef1c153411
ShiftyFella <31674713+ShiftyFella@users.noreply.github.com>
2017-09-12 22:24:55 -0400

    Created publishing in server content of package.json file

--- index.js
+++ index.js
@@ -1,19 +1,18 @@
 var http = require('http');
+var fs = require('fs');
 
 var server = http.createServer(function (request, response) {
     response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end("Hello World\n");
-
+    fs.readFile('package.json', 'utf8', function (err, data) {
+        if (err) {
+            response.end(data);
+            return console.log(err);
+        }
+        response.end("Hello World\n" + data);
+        console.log(data);
+    });
 });
 
 server.listen(8000, '127.0.0.1');
+
 console.log("Server listens on 127.0.0.1:8000");
-
-fs = require('fs');
-fs.readFile('package.json', 'utf8', function (err, data) {
-    if (err) {
-        return console.log(err);
-    }
-    console.log(data);
-
-});
